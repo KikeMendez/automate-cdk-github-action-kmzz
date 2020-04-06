3ca9b08f6245e7fe9aabe3429468367142a35e52
Enrique Mendez <enrique.mendez@endemolshine.com>
2020-04-06 23:02:48 +0100

    amend workflow

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,7 @@ LABEL maintainer="Enrique Mendez <kikemnzz@gmail.com>"
 RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
 RUN unzip awscliv2.zip
-
 RUN ./aws/install
-RUN npm install -g aws-cdk
 
 COPY entrypoint.sh /usr/local/bin
 RUN chmod +x /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,51 +1,45 @@
 #!/bin/bash
-echo "${PWD}"
 
-printenv;
+set -o errexit
 
 # Validate AWS Credentials
-  if [ "${AWS_ACCESS_KEY_ID}" == "" ]; then
-   	echo "Input AWS_ACCESS_KEY_ID cannot be empty"
-		exit 1
-  fi
 
-  if [ "${AWS_SECRET_ACCESS_KEY}" == "" ]; then
-   	echo "Input AWS_SECRET_ACCESS_KEY cannot be empty"
-		exit 1
-  fi
+if [ "${AWS_ACCESS_KEY_ID}" == "" ]; then
+  echo "Input AWS_ACCESS_KEY_ID cannot be empty"
+  exit 1
+fi
 
-# Configure aws cli with a default profile
-aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}";
-aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}";
-cat ~/.aws/credentials
-aws s3 ls
-ls -a
-cdk bootstrap;
+if [ "${AWS_SECRET_ACCESS_KEY}" == "" ]; then
+  echo "Input AWS_SECRET_ACCESS_KEY cannot be empty"
+  exit 1
+fi
 
-#
-#GITHUB_HEAD_REF=
-#GITHUB_ACTOR=KikeMendez
-#GITHUB_ACTION=KikeMendezcdk-github-action-kmzz
-#PWD=/github/workspace
-#HOME=/github/home
-#RUNNER_TEMP=/home/runner/work/_temp
-#RUNNER_WORKSPACE=/home/runner/work/action-cdk-test
-#GITHUB_REF=refs/heads/master
-#GITHUB_SHA=878d1ae02a262d7c41f4649995dee7f2d368a87b
-#ACTIONS_RUNTIME_TOKEN=***
-#GITHUB_RUN_ID=71355398
-#NODE_VERSION=13.12.0
-#GITHUB_EVENT_PATH=/github/workflow/event.json
-#RUNNER_OS=Linux
-#GITHUB_BASE_REF=
-#INPUT_CDK_ACTION=bootstrap
-#AWS_ACCESS_KEY_ID=***
-#SHLVL=1
-#GITHUB_REPOSITORY=KikeMendez/action-cdk-test
-#GITHUB_EVENT_NAME=push
-#GITHUB_RUN_NUMBER=7
-#GITHUB_WORKFLOW=CDK Test
-#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-#GITHUB_WORKSPACE=/github/workspace
-#ACTIONS_RUNTIME_URL=https://pipelines.actions.githubusercontent.com/fSOm4ZJu1qt3gPeod8pgAF7xOrNK7G2bKPhhWIQA1lK3ndwbr0/
-#ACTIONS_CACHE_URL=https://artifactcache.actions.githubusercontent.com/fSOm4ZJu1qt3gPeod8pgAF7xOrNK7G2bKPhhWIQA1lK3ndwbr0/
+if [ "${AWS_DEFAULT_REGION}" == "" ]; then
+  echo "Input AWS_DEFAULT_REGION cannot be empty"
+  exit 1
+fi
+
+if [ "${INPUT_CDK_ACTION}" == "" ]; then
+  echo "Input INPUT_CDK_ACTION cannot be empty"
+  exit 1
+fi
+
+# Configure AWS cli with a default profile
+aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
+aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
+aws configure set default.region "${AWS_DEFAULT_REGION}"
+
+## Install AWS CDK
+[[ -z "${INPUT_CDK_VERSION}" ]] && npm install -g aws-cdk || npm install -g aws-cdk@"${INPUT_CDK_VERSION}"
+
+echo "cdk version:"
+cdk --version
+
+# Run cdk for a specific stack
+if [[ "${INPUT_CDK_STACK_DIRECTORY}" != '' && "${INPUT_CDK_STACK}" != '' ]]; then
+  cd "${GITHUB_WORKSPACE}"/"${INPUT_CDK_STACK_DIRECTORY}"
+  cdk ${INPUT_CDK_ACTION} ${INPUT_CDK_STACK}
+  exit 0;
+fi
+
+cdk ${INPUT_CDK_ACTION}
